b56a3a284d84163e358fbab5c63324e295b48456
Sean Birmingham <seannbirmingham98@gmail.com>
2019-11-20 00:41:30 -0500

    set up component, fetched data from API

---
+++ src/components/Photo.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default function Photo() {
+
+
+    return (
+        <div>
+            <h1>NASA Photo of the Day</h1>
+            <h2>photo title</h2>
+            <img src="" alt="random image from NASA" />
+            <div>
+                <p>explanation</p>
+                <p>date</p>
+                <p>copyright</p>
+            </div>
+        </div>
+    )
+
+}
